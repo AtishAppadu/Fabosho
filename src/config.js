f2902d3b0e44e1c750517f6f41c1ebf0ac453520
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-03-06 23:57:13 +0000

    test v5

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 window._config = {
-    cognito: {
+    cognito: { 
         userPoolId: 'eu-west-2_dFTg6o34G', // e.g. us-east-2_uXboG5pAb
         region: 'eu-west-2', // e.g. us-east-2
         clientId: '4bdnlrt9gfpgou2asb4fk9vd9q' //is this used anywhere?
