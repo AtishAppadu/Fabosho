77bfe00afcb93a25d2d01aabdb321997f1d6be64
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-03-06 23:08:55 +0000

    Cognito implementation

---
+++ src/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_dFTg6o34G', // e.g. us-east-2_uXboG5pAb
+        region: 'eu-west-2', // e.g. us-east-2
+        clientId: '4bdnlrt9gfpgou2asb4fk9vd9q' //is this used anywhere?
+        },
+    };
