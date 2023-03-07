cf72e7288b57c69a70f8fc662b6bd125bd99797f
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-03-07 01:00:56 +0000

    test v7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
 
-function App({ signOut }) {
+import React from 'react';
+import "./App.css";
+import UserPool from "./UserPool";
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+
+const App = () => {
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div>
+      <Signup />
+      <Login />
+    </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
