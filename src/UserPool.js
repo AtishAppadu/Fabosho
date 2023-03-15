4f060a47083abbdd34519def28841627346ef550
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-03-15 16:54:11 +0000

    delete

--- src/UserPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-    UserPoolId: "eu-west-2_dFTg6o34G",  // Your user pool id here
-    ClientId: "4bdnlrt9gfpgou2asb4fk9vd9q" // Your client id here
-};
-
-export default new CognitoUserPool(poolData);
