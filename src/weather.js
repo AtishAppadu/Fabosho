e80b7939a605134e29bffefe5b2587c7d5974cf6
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-04-26 18:29:16 +0100

    test1

---
+++ src/weather.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const weather = () => {
+    return (
+        <div className='container mx-auto bg-red-50'>
+            mopanpcaa
+        </div>
+    )
+}
+
+export default weather
