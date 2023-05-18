678690dad97ffa974e4f17d94e2f6b7e8a516e32
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-05-18 23:28:40 +0100

    added more images

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,6 @@
 function generator() {
     var x=Math.floor((Math.random()*3)+1);
     console.log(x);
-    document.getElementById("divImage").innerHTML=`
-        <img src="images/clothing${x}.jpg" alt="image" width="300px" height="300px">`;
+    document.getElementById("clothingImage").innerHTML=`
+        <img src="images/Cfiles/clothing${x}.jpg" alt="image" width="300px" height="300px">`;
 }
