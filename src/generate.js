4228ddde95ea469734829e42d8e88ff654a6aa94
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-05-18 23:58:49 +0100

    notes within code

--- src/generate.js
+++ src/generate.js
@@ -1,19 +1,26 @@
+
+// Male Clothing Generator
 function malegenerator() {
-    var x=Math.floor((Math.random()*2)+1);
+
+        // Male Clothing Cap
+    var x=Math.floor((Math.random()*2)+1);  // Random Number Generator  that generates a number between 1 and 2
     console.log(x);
     document.getElementById("MaleClothingImageCap").innerHTML=`
         <img src="images/Cfiles/Men/ExampleCapClothing${x}.jpg" alt="image" width="300px" height="300px">`;
 
+        // Male Clothing Top
     var x=Math.floor((Math.random()*2)+1);
     console.log(x);
     document.getElementById("MaleClothingImageTop").innerHTML=`
         <img src="images/Cfiles/Men/ExampleTopClothing${x}.jpg" alt="image" width="300px" height="300px">`;
 
+        // Male Clothing Bottom
     var x=Math.floor((Math.random()*2)+1);
     console.log(x);
     document.getElementById("MaleClothingImageBottom").innerHTML=`
         <img src="images/Cfiles/Men/ExampleBottomClothing${x}.jpg" alt="image" width="300px" height="300px">`;
-
+        
+        // Male Clothing Shoes
     var x=Math.floor((Math.random()*2)+1);
     console.log(x);
     document.getElementById("MaleClothingImageShoes").innerHTML=`
@@ -21,21 +28,26 @@ function malegenerator() {
     }
 
 function femalegenerator() {
-    var x=Math.floor((Math.random()*3)+1);
+
+        // Female Clothing Cap
+    var x=Math.floor((Math.random()*3)+1);    // Random Number Generator  that generates a number between 1 and 3
     console.log(x);
     document.getElementById("FemaleClothingImageCap").innerHTML=`
         <img src="images/Cfiles/Women/ExampleCapClothing${x}.jpg" alt="image" width="300px" height="300px">`;
-
+        
+        // Female Clothing Top
     var x=Math.floor((Math.random()*2)+1);
     console.log(x);
     document.getElementById("FemaleClothingImageTop").innerHTML=`
         <img src="images/Cfiles/Women/ExampleTopClothing${x}.jpg" alt="image" width="300px" height="300px">`;
-
+        
+        // Female Clothing Bottom
     var x=Math.floor((Math.random()*2)+1);
     console.log(x);
     document.getElementById("FemaleClothingImageBottom").innerHTML=`
         <img src="images/Cfiles/Women/ExampleBottomClothing${x}.jpg" alt="image" width="300px" height="300px">`;
-
+        
+        // Female Clothing Shoes
     var x=Math.floor((Math.random()*2)+1);
     console.log(x);
     document.getElementById("FemaleClothingImageShoes").innerHTML=`
