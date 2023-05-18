bcf0f865bdbbc4a2b966075eba9e84bd3d31bcad
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2023-05-18 22:43:12 +0100

    changes from "img" to "images"

--- src/generate.js
+++ src/generate.js
@@ -2,5 +2,5 @@ function generator() {
     var x=Math.floor((Math.random()*3)+1);
     console.log(x);
     document.getElementById("divImage").innerHTML=`
-        <img src="img/clothing${x}.jpg" alt="image" width="300px" height="300px">`;
+        <img src="images/clothing${x}.jpg" alt="image" width="300px" height="300px">`;
 }
